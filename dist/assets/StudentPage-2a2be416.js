d39bd5b7763a24a705962a4c133e8e8f60651b2c
abduhodi <tursunboyev.abduhodi1997@mail.ru>
2024-02-09 23:52:26 +0500

    update dist

--- dist/assets/StudentPage-2a2be416.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,b as n,d as o}from"./index-5fceaf5e.js";const c={},s=o("h1",null,"Student",-1),_=[s];function a(r,d){return t(),n("div",null,_)}const u=e(c,[["render",a]]);export{u as default};
